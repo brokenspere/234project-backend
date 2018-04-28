386beac70c8c346dae5a6898a5eaf4832be50bd6
Unknown <nattapat013@outlook.com>
2018-04-28 15:35:56 +0700

    Add testGetAllproductWithMock
    add this method in ProductServiceImplTest .

---
+++ src/test/java/camt/se234/project/ProductServiceImplTest.java
@@ -0,0 +1,50 @@
+package camt.se234.project;
+import camt.se234.project.dao.ProductDao;
+import camt.se234.project.entity.Product;
+import camt.se234.project.service.ProductService;
+import camt.se234.project.service.ProductServiceImpl;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasItem;
+import static org.hamcrest.Matchers.hasItems;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.nullValue;
+import static org.hamcrest.number.IsCloseTo.closeTo;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class ProductServiceImplTest {
+    ProductDao productDao;
+    ProductServiceImpl productService;
+
+
+    @Before
+    public void setup(){
+
+        productDao = mock(ProductDao.class);
+        productService = new ProductServiceImpl() ;
+        productService.setProductDao(productDao);
+        }
+
+     @Test
+    public void testGetAllproductWithMock(){
+        List<Product> mockProduct = new ArrayList<>();
+        mockProduct.add(new Product("123","Carrot","See som","xxx",50));
+        mockProduct.add(new Product("124 ","Orange","See som","xxx",60));
+        mockProduct.add(new Product("125","Grape","See muang","xxx",80));
+        when(productDao.getProducts()).thenReturn(mockProduct);
+        assertThat(productService.getAllProducts(),hasItems(new Product("123","Carrot","See som","xxx",50),
+                new Product("124 ","Orange","See som","xxx",60),new Product("125","Grape","See muang","xxx",80)));
+     }
+
+    
+
+    }
+
+
+
